f47bb252ac7230b239bc9d8ae2f41c721aa4a1f3
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-05-30 07:09:57 +0800

    Delete js正则表达式截取函数.js

--- js正则表达式截取函数.js
+++
@@ -1,5 +0,0 @@
-String.prototype.sj=function(val,val2){try{var reg=new RegExp(val+"(.*?)"+val2);var value=String(this).match(reg)[1];if(typeof(JSON.parse(value))=="object"){return JSON.parse(value)}else{return value}}catch(err){if(this){return this;}else{return false}}};
-
-
-var str="我是一个字符串演示啊";
-alert(str.sj("是","演")); //输出 “一个字符串”

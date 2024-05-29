3a8f9d733b019fc2d29588fe0cb45a962507c92d
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-05-30 07:10:54 +0800

    update iapp sj截取字符函数.js

---
+++ iapp sj截取字符函数.js
@@ -0,0 +1,21 @@
+String.prototype.sj = function(val, val2) {
+    try {
+        var reg = new RegExp(val + "(.*?)" + val2);
+        var getval = String(this).match(reg)[1];
+        if (typeof(JSON.parse(getval)) == "object") {
+            return JSON.parse(getval)
+        } else {
+            return getval;
+        }
+    } catch (err) {
+        if (getval) {
+            return getval;
+        } else {
+            return false
+        }
+    }
+};
+
+
+//var str = "我是一个字符串演示啊";
+//alert(str.sj("是", "演")); //输出 “一个字符串”
